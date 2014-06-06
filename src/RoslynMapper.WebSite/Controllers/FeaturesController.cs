77d5e1483c1af1330e37a03ea619ca5f48b7436c
liuhongbo <liuhongbo@live.com>
2014-06-05 23:34:56 -0400

    website more conent

---
+++ src/RoslynMapper.WebSite/Controllers/FeaturesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RoslynMapper.WebSite.Controllers
+{
+    public class FeaturesController : Controller
+    {
+        // GET: Features
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/RoslynMapper.WebSite/Controllers/HowItWorksController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RoslynMapper.WebSite.Controllers
+{
+    public class HowItWorksController : Controller
+    {
+        // GET: HowItWorks
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
